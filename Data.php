2c2e9d784b06f3c37f23343fd10441b2ad57ef6c
Nozawa <n.shota1115@gmail.com>
2018-02-05 23:04:17 +0900

    update Data.php

--- Data.php
+++ Data.php
@@ -1,15 +1,13 @@
 <?php
-	$path = "Data.json";
-	$data = file_get_contents($path);
-	$json = json_decode($data,true);
-	if(array_key_exists('data',$_GET)){
-	$data = json_decode($_GET['data'],true);
-	$json[] = [''=>,''=>]
-	array.push($data,["id"=>$id])
-	file_put_contents($path,$_GET['data'],FILE_APPEND);
-	}else{
-	$data = file_get_contents($path);
-	echo json_encode($data);
-	}
+	$path = "data.json";
+	$json = file_get_contents($path);
+	$array = json_decode($json,true);
 
+	if(array_key_exists('data',$_GET)){
+		$data = json_decode($_GET['data'],true);
+		array.push($array,$data)
+		file_put_contents($path,json_encode($array));
+	}else{
+		echo $json;
+	}
 ?>
